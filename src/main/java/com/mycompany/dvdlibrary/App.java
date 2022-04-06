a92f43feba7a3a217e57ee812152cfdfd50e3098
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2022-04-06 10:47:07 -0400

    organized into packages

---
+++ src/main/java/com/mycompany/dvdlibrary/App.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.dvdlibrary;
+
+/**
+ *
+ * @author chica
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- src/main/java/com/mycompany/dvdlibrary/ui/UserIOConsoleImpl.java
+++ src/main/java/com/mycompany/dvdlibrary/ui/UserIOConsoleImpl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.dvdlibrary;
+package com.mycompany.dvdlibrary.ui;
 import java.util.Scanner;
 /**
  *
